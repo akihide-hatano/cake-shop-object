cf70f445e6ad2fec0b06ff7ebc683f6ff08cd9a4
akihide-hatano <a.h.19841202@gmail.com>
2025-12-14 11:18:09 +0900

    app.javaにcakeの種類を追加。

--- src/main/java/com/cakeshop/App.java
+++ src/main/java/com/cakeshop/App.java
@@ -7,11 +7,19 @@ import com.cakeshop.pricing.PricePolicy;
 
 public class App {
     public static void main(String[] args){
-        Product cake = new Cake("フルーツタルト", 600,new NormalPrice());
+    Product cake1 = new Cake("フルーツショート", 520, new NormalPrice());
+    Product cake2 = new Cake("ベリーミックスケーキ", 580, new NormalPrice());
+    Product cake3 = new Cake("チョコレートケーキ", 550, new NormalPrice());
+    Product cake4 = new Cake("ガトーショコラ", 600, new NormalPrice());
+    Product cake5 = new Cake("アイスケーキ", 650, new NormalPrice());
+    Product cake6 = new Cake("クッキーアイスサンド", 480, new NormalPrice());
+    Product cake7 = new Cake("ショートケーキ", 500, new NormalPrice());
+    Product cake8 = new Cake("モンブラン", 620, new NormalPrice());
+    Product cake9 = new Cake("チーズケーキ", 580, new NormalPrice());
+    Product cake10 = new Cake("ティラミス", 560, new NormalPrice());
+
+
         PricePolicy pricePolicy = new NormalPrice();
-
-        int price = pricePolicy.calculate(600);
-
-        System.out.println(cake.getName()+"の価格は"+price+"円です。");
+        System.out.println(cake3.getName()+"の価格は"+cake3.getPrice()+"円です。");
     }
 }
